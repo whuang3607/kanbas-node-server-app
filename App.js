3441c1b2b6cdfb6056cd54f03dfe6399ead10ebd
whuang3607 <floridian567@gmail.com>
2024-04-01 16:53:09 -0400

    finished A5

--- App.js
+++ App.js
@@ -3,11 +3,15 @@ import Hello from "./Hello.js"
 import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
+import AssignmentRoutes from './Kanbas/assignments/routes.js';
 
 const app = express()
 app.use(cors());
 app.use(express.json());
 CourseRoutes(app);
+ModuleRoutes(app);
+AssignmentRoutes(app);
 Lab5(app);
 Hello(app)
-app.listen(4000)
+app.listen(process.env.PORT || 4000);
--- Kanbas/Database/modules.js
+++ Kanbas/Database/modules.js
@@ -217,8 +217,8 @@ export default [
     },
     {
       "_id": "M1",
-      "name": "Week 1 - HTML",
-      "description": "Learning the basics of HTML",
+      "name": "Week 1 - Intro to Network Fundamentals",
+      "description": "Learning the basics of Network Fundamentals",
       "course": "CS2",
       "lessons": [
         {
@@ -235,20 +235,20 @@ export default [
         },
         {
           "_id": "M1L3",
-          "name": "Learn what is Web Development",
-          "description": "Finding out what Web Development is",
+          "name": "Learn what is a Computer Network",
+          "description": "Finding out what Computer Networks are",
           "module": "M1"
         },
         {
           "_id": "M1L4",
-          "name": "Creating a development environment",
-          "description": "Create a development environment",
+          "name": "Creating sockets in Python",
+          "description": "Create a socket in Python",
           "module": "M1"
         },
         {
           "_id": "M1L5",
-          "name": "Creating a Web Application",
-          "description": "Create Web Application",
+          "name": "Connecting to a server in Python",
+          "description": "Connect to class server in Python",
           "module": "M1"
         },
         {
@@ -265,13 +265,13 @@ export default [
         },
         {
           "_id": "M1L8",
-          "name": "Full Stack Developer - Chapter 1 - Introduction",
+          "name": "Computer Networks: A Systems Approach - Chapter 1",
           "description": "Chapter 1 of textbook",
           "module": "M1"
         },
         {
           "_id": "M1L9",
-          "name": "Full Stack Developer - Chapter 2 - Creating User Interfaces with HTML",
+          "name": "Computer Networks: A Systems Approach - Chapter 2",
           "description": "Chapter 2 of textbook",
           "module": "M1"
         },
@@ -283,22 +283,22 @@ export default [
         },
         {
           "_id": "M1L11",
-          "name": "Introduction to Web Development",
-          "description": "Slides for introducing Web Development",
+          "name": "Introduction to Computer Networks",
+          "description": "Slides for introducing Computer Networks",
           "module": "M1"
         },
         {
           "_id": "M1L12",
-          "name": "Creating an HTTP server with Node.js",
-          "description": "Slides for creating an HTTP server with Node.js",
+          "name": "History of Computer Networks",
+          "description": "Slides for the history of computer networks",
           "module": "M1"
         }
       ]
     },
     {
       "_id": "M2",
-      "name": "Week 2 - Formatting User Interfaces with HTML",
-      "description": "Create web page using HTML",
+      "name": "Week 2 - Computer Networks Architecture",
+      "description": "Understanding the underlying architecture of computer networks",
       "course": "CS2",
       "lessons": [
         {
@@ -309,20 +309,20 @@ export default [
         },
         {
           "_id": "M2L2",
-          "name": "Learn how to create user interfaces with HTML",
-          "description": "Create web page with HTML",
+          "name": "Learn the OSI Model",
+          "description": "Learn the OSI Model",
           "module": "M2"
         },
         {
           "_id": "M2L3",
-          "name": "Keep working on assignment 1",
-          "description": "Work on Assignment 1",
+          "name": "Physical Layer and Data Link Layer",
+          "description": "Learn the physical layer and the data link layer of the OSI model",
           "module": "M2"
         },
         {
           "_id": "M2L4",
-          "name": "Deploy the assignment to Netlify",
-          "description": "Deploy assignment to Netlify",
+          "name": "Keep working on Assignment 1",
+          "description": "Work on Assignment 1",
           "module": "M2"
         },
         {
@@ -333,14 +333,14 @@ export default [
         },
         {
           "_id": "M2L6",
-          "name": "Full Stack Developer - Chapter 1 - Introduction",
-          "description": "Chapter 1 of textbook",
+          "name": "Computer Networks: A Systems Approach - Chapter 2",
+          "description": "Chapter 2 of textbook",
           "module": "M2"
         },
         {
           "_id": "M2L7",
-          "name": "Full Stack Developer - Chapter 2 - Creating User Interfaces with HTML",
-          "description": "Chapter 2 of textbook",
+          "name": "Computer Networks: A Systems Approach - Chapter 3",
+          "description": "Chapter 3 of textbook",
           "module": "M2"
         },
         {
