39a8b1faa61b16f3608c84a86c7a9b464e62a04b
whuang3607 <floridian567@gmail.com>
2024-04-12 13:43:44 -0400

    added mongoose

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5.js";
 import cors from "cors";
@@ -6,6 +7,7 @@ import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from './Kanbas/assignments/routes.js';
 
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas")
 const app = express()
 app.use(cors());
 app.use(express.json());
