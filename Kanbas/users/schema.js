f6f92634c491edac13bb60dfbdbddf66f7bab8f1
whuang3607 <floridian567@gmail.com>
2024-04-16 23:58:17 -0400

    this is for modules

--- Kanbas/users/schema.js
+++ Kanbas/users/schema.js
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
-    id: {type: String, required: true, unique: true},
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    firstName: String,
-    lastName: String,
-    email: String,
-    dob: Date,
-    role: {
-      type: String,
-      enum: ["STUDENT", "FACULTY", "ADMIN", "USER"],
-      default: "USER",},
-  },
-  { collection: "users" }
+  id: {type: String, required: true, unique: true},
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  firstName: String,
+  lastName: String,
+  email: String,
+  dob: Date,
+  role: {
+    type: String,
+    enum: ["STUDENT", "FACULTY", "ADMIN", "USER"],
+    default: "USER",},
+},
+{ collection: "users" }
 );
 export default userSchema;
