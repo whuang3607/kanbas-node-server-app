579d7f4ec83ceeba99b4c15dd819b39445d156ce
whuang3607 <floridian567@gmail.com>
2024-04-17 18:09:23 -0400

    went back to previous commit

--- Kanbas/courses/routes.js
+++ Kanbas/courses/routes.js
@@ -1,7 +1,5 @@
 import Database from "../Database/index.js";
-
 export default function CourseRoutes(app) {
-  
   app.get("/api/courses/:id", (req, res) => {
     const { id } = req.params;
     const course = Database.courses
